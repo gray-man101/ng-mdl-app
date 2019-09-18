800905adbcb46518696620ee1b13b9132369300a
Aleksej Voroncov <a.voroncovs@bill.me>
2019-09-18 16:35:52 +0300

    kitaec s telefonom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from "./app.component";
 import { CompanyComponent } from "./company/company.component";
+import { HomeComponent } from "./home/home.component";
 
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'company/:id', component: CompanyComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyComponent } from './company/company.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompanyComponent
+    CompanyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
