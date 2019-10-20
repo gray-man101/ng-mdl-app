9a29e90d01eada76fb09e55f158b5d02954d4a72
Leha <leha@leha.com>
2019-10-20 22:56:16 +0300

    shopping cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CompanyComponent } from "./company/company.component";
-import { HomeComponent } from "./home/home.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {CompanyComponent} from "./company/company.component";
+import {HomeComponent} from "./home/home.component";
+import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'company/:id', component: CompanyComponent }
+  {path: '', component: HomeComponent},
+  {path: 'company/:id', component: CompanyComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {AppComponent} from './app.component';
 import {CompanyComponent} from './company/company.component';
 import {HomeComponent} from './home/home.component';
 import {MdlSnackbarModule, MdlTextFieldModule} from "@angular-mdl/core";
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyComponent,
-    HomeComponent
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
